175829bb68d8c0ca71c94ce72837507e33c8ce1a
IanTeddy <meihirata12@gmail.com>
2024-02-13 16:34:02 -0500

    Add instance variables

--- Patient.cs
+++ Patient.cs
@@ -4,7 +4,9 @@ namespace Csharp_Assignment_1
 	public class Patient
 	{
 		private string firstName;
-
+		private string lastName;
+		private Double weight;
+		private Double height;
 
 		public Patient()
 		{
