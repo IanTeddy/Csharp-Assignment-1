f9bd728f64ebb550ade1a5dd903b06dbaa099037
IanTeddy <meihirata12@gmail.com>
2024-02-13 16:56:17 -0500

    Add properties

--- Patient.cs
+++ Patient.cs
@@ -4,7 +4,6 @@ namespace Csharp_Assignment_1
 	public class Patient
 	{
 		private string firstName;
-
 		private string lastName;
 		private Double weight;
 		private Double height;
@@ -12,6 +11,11 @@ namespace Csharp_Assignment_1
 		public Patient()
 		{
 		}
-	}
+
+        public double Height { get => height; set => height = value; }
+        public double Weight { get => weight; set => weight = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+    }
 }
 
